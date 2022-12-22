9d0ae28a43f96589aa0010698f594840a4fe6b67
lemoncoat <joy961027@naver.com>
2022-12-22 11:53:16 +0900

    2022-12-22 main.jsp수정

--- src/main/java/com/champion/deliciousInfo/food/repository/FoodNutrientMapper.java
+++ src/main/java/com/champion/deliciousInfo/food/repository/FoodNutrientMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
-public interface wFoodNutrientMapper {
+public interface FoodNutrientMapper {
 
     List<FoodNutrient> findAll();
     FoodNutrient findOne(int foodNo);
