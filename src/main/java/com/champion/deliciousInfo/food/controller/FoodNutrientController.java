644e17554dd2c5fabc9cf2176436c4d584a8c853
lemoncoat <joy961027@naver.com>
2022-12-26 10:59:26 +0900

    2022-12-26 추천 알고리즘 유클리드 기법 적용

--- src/main/java/com/champion/deliciousInfo/food/controller/FoodNutrientController.java
+++ src/main/java/com/champion/deliciousInfo/food/controller/FoodNutrientController.java
@@ -44,7 +44,6 @@ public class FoodNutrientController {
         model.addAttribute("sl",sl);
 
         log.info("foodnutrient - {}", foodNutrient);
-        log.info("recommendList - {}", todayNutrientList);
         return "food/food-nutrient";
 
     }
