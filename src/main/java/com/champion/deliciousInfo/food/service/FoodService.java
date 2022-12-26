ad14ba1e9ee91d91e8b48237b6400cd68fa10337
lemoncoat <joy961027@naver.com>
2022-12-26 14:13:49 +0900

    2022-12-26 excel 변경

--- src/main/java/com/champion/deliciousInfo/food/service/FoodService.java
+++ src/main/java/com/champion/deliciousInfo/food/service/FoodService.java
@@ -102,6 +102,7 @@ public class FoodService {
             //db에 저장
             flag1 = foodMapper.save(foodnutrient.getFood());
             flag2 = foodNutrientMapper.save(foodnutrient);
+
         }
 
         return flag1 && flag2;
